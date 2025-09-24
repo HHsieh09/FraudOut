e54f7cc8667ff1c8515a20e109acd5bd2c471aa6
HHsieh09 <henryfbs01@gmail.com>
2025-09-24 00:13:45 -0400

    Feature: Revise spark_to_hdfs.py to follow the original hdfs plan (docs/hdfs_plan.md)

--- docs/hdfs_plan.md
+++ docs/hdfs_plan.md
@@ -1,21 +1,21 @@
 ### For raw json data
-/data/fraudout/raw/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.json
+user/spark/fraudout/raw/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.json
 
 ### For Spark-cleaned data
-/data/fraudout/curated/tx_curated/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
+user/spark/fraudout/curated/tx_curated/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
 
 ### For feature table such as velocity, geographical distance, and holiday flags
-/data/fraudout/curated/tx_features/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
+user/spark/fraudout/curated/tx_features/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
 
 ### For scoring results from streaming rules or models, including score and decision
-/data/fraudout/curated/tx_scores/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
+user/spark/fraudout/curated/tx_scores/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
 
 ### For Deequ's data quality check
-/data/fraudout/curated/dq_results/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
+user/spark/fraudout/curated/dq_results/yyyy=YYYY/mm=MM/dd=DD/hour=HH/*.parquet
 
 ### Usage
-`hdfs df -mkdir -p /data/fraudout/raw`
-`hdfs df -mkdir -p /data/fraudout/curated/tx_curated`
-`hdfs df -mkdir -p /data/fraudout/curated/tx_features`
-`hdfs df -mkdir -p /data/fraudout/curated/tx_scores`
-`hdfs df -mkdir -p /data/fraudout/curated/dq_results`
+`hdfs df -mkdir -p user/spark/fraudout/raw`
+`hdfs df -mkdir -p user/spark/fraudout/curated/tx_curated`
+`hdfs df -mkdir -p user/spark/fraudout/curated/tx_features`
+`hdfs df -mkdir -p user/spark/fraudout/curated/tx_scores`
+`hdfs df -mkdir -p user/spark/fraudout/curated/dq_results`
--- spark/spark_to_hdfs.py
+++ spark/spark_to_hdfs.py
@@ -1,12 +1,20 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import current_timestamp, get_json_object, col, date_format
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import current_timestamp, get_json_object, col, date_format, from_json,  window, count, sum as _sum, lit, when, to_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 import os
 
+# Define Kafka parameters
 KAFKA_BOOTSTRAP_SERVERS = "kafka:9092" 
 KAFKA_TOPICS = "fraudout.txn.raw"
 
-OUTPUT_PATH = "hdfs://namenode:8020/user/spark/fraudout/bronze"
+# Define HDFS paths
+RAW_PATH = "hdfs://namenode:8020/user/spark/fraudout/raw/"
+CURATED_PATH = "hdfs://namenode:8020/user/spark/fraudout/curated/tx_curated"
+FEATURES_PATH = "hdfs://namenode:8020/user/spark/fraudout/curated/tx_features"
+SCORES_PATH = "hdfs://namenode:8020/user/spark/fraudout/curated/tx_scores"
+DQ_PATH = "hdfs://namenode:8020/user/spark/fraudout/curated/dq_results"
+
+# Define checkpoint paths
 CHECKPOINT_DIR = "hdfs://namenode:8020/user/spark/fraudout/checkpoints"
 
 # Create Spark session
@@ -19,19 +27,34 @@ spark = SparkSession.builder \
 # Set log level to WARN to reduce verbosity
 spark.sparkContext.setLogLevel("WARN")
 
+# Define the schema for the incoming JSON data.
+# Only define the top-level fields; nested fields are extracted using get_json_object
+# This schema matches the structure of the JSON messages in the Kafka topic
+
+txn_schema = StructType([
+    StructField("type", StringType()),
+    StructField("token", StringType()),
+    StructField("amount", DoubleType()),          # 若原始 JSON 有時是字串，改成 StringType 再 cast
+    StructField("network", StringType()),
+    StructField("created_time", StringType()),
+    StructField("user_transaction_time", StringType()),
+    StructField("currency_code", StringType()),
+])
+
 # Read from Kafka topic
 # option("kafka.bootstrap.servers") specifies the Kafka server address
 # option("subscribe") specifies the Kafka topic to subscribe to
 # option("startingOffsets") specifies where to start reading messages (latest or earliest)
-df = spark.readStream.format("kafka") \
+source = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers",KAFKA_BOOTSTRAP_SERVERS) \
     .option("subscribe", KAFKA_TOPICS) \
-    .option("stratingPffsets", "latest") \
+    .option("startingOffsets", "latest") \
     .load()
 
+# Sink 1: raw json data to HDFS
 # Select the value from the Kafka message and cast it to string
 # The value is the actual message content
-kafka_df = df.selectExpr("CAST(key as STRING) as event_key",
+raw_df = source.selectExpr("CAST(key as STRING) as event_key",
                         "CAST(value as STRING) as event_json",
                         "timestamp as kafka_ts") \
             .withColumn("ingestion_ts", current_timestamp()) 
@@ -39,25 +62,106 @@ kafka_df = df.selectExpr("CAST(key as STRING) as event_key",
             # ingestion_ts means the time when the message was ingested into Spark
             # They are used to measure the end-to-end latency
 
-# Define the schema for the incoming JSON data
-# This schema matches the structure of the JSON messages in the Kafka topic
-txn_schema = StructType([
-    StructField("type", StringType()),
-    StructField("country", StringType()),
-    StructField("merchant", StringType()),
-    StructField("token", StringType()),
-    StructField("amount", StringType()),
-    StructField("network", StringType()),
-    StructField("channel", StringType()),
-    StructField("created_time", StringType()),
-    StructField("user_transaction_time", StringType()),
-    StructField("currency_code", StringType()),
-])
+raw_df = raw_df.withColumn("dt", date_format(col("ingestion_ts"), "yyyy-MM-dd"))
 
+# Write the raw data to HDFS in JSON format
+raw_query = raw_df.writeStream \
+    .format("json") \
+    .option("path", RAW_PATH) \
+    .option("checkpointLocation", f"{CHECKPOINT_DIR}/raw") \
+    .partitionBy("dt") \
+    .outputMode("append") \
+    .start()
+
+# Sink 2: curated parquet data to HDFS
+# Parse the JSON data using the defined schema and flatten the structure
+# withColumn("txn", from_json(...)) creates a new column "txn" by parsing the JSON string
+# select("event_key","event_json","kafka_ts", "ingestion_ts", "txn.*") selects the original columns and flattens the "txn" struct into individual columns
+curated_df = raw_df.withColumn("txn", from_json(col("event_json"), txn_schema)) \
+    .select("event_key","event_json","kafka_ts", "ingestion_ts", "dt", "txn.*",
+        get_json_object("event_json", "$.card_acceptor.name").alias("merchant"),
+        get_json_object("event_json", "$.card_acceptor.country_code").alias("country"),
+        get_json_object("event_json", "$.transaction_metadata.payment_channel").alias("channel"))
+
+# Write the curated data to HDFS in Parquet format
+curated_query = curated_df.writeStream \
+    .format("parquet") \
+    .option("path", CURATED_PATH) \
+    .option("checkpointLocation", f"{CHECKPOINT_DIR}/curated") \
+    .partitionBy("dt") \
+    .outputMode("append") \
+    .start()
+
+# Sink 3: feature engineered data to HDFS
+features_base = curated_df\
+    .withColumn("amount_double", col("amount").cast("double")) \
+    .withColumn("event_ts", to_timestamp(col("user_transaction_time")))
+
+# Perform feature engineering by aggregating transaction counts and amounts over a 5-minute window
+# withColumn("amount_double", col("amount").cast(DoubleType())) casts the amount to DoubleType for aggregation
+# withColumn("event_ts", col("user_transaction_time").cast(TimestampType())) casts the user_transaction_time to TimestampType for windowing
+# withWatermark("event_ts","10 minutes") sets a watermark on event_ts to handle late data. Data older than 10 minutes will be ignored
+# groupBy(col("token"), window(col("event_ts", "5 minutes"))) groups the data by token and 5-minute window
+# agg(count("*").alias("txn_count_5min"), _sum("amount_double").alias("txn_amount_5min")) computes the count and sum of transactions in each group
+# withColumn("dt", date_format(col("window.end"), "yyyy-MM-dd")) creates a date column for partitioning the output data. The date is based on the end of the window
+features_df = features_base.withWatermark("event_ts","10 minutes") \
+    .groupBy(
+        col("token"),
+        window(col("event_ts"), "5 minutes")
+    ).agg(
+        count("*").alias("txn_count_5min"),
+        _sum("amount_double").alias("txn_amount_5min")
+    ) \
+    .withColumn("dt", date_format(col("window.end"), "yyyy-MM-dd"))
+
+# Write the feature engineered data to HDFS in Parquet format
+feature_query = features_df.writeStream \
+    .format("parquet") \
+    .option("path", FEATURES_PATH) \
+    .option("checkpointLocation", f"{CHECKPOINT_DIR}/features") \
+    .partitionBy("dt") \
+    .outputMode("append") \
+    .start()
+
+# Sink 4: scores data to HDFS
+# Simple scoring logic: if amount > 1000, score = 1 (fraud), else score = 0 (not fraud)
+# withColumn("score", ...) creates a new column "score" based on the amount
+# withColumn("decision", ...) creates a new column "decision" based on the amount
+scores_df = curated_df.withColumn("score", (col("amount").cast(DoubleType()) > 1000).cast("int")) \
+    .withColumn("decision", when(col("amount").cast("double") > 1000, lit("REVIEW")).otherwise(lit("OK"))) \
+    .withColumn("dt", date_format(col("ingestion_ts"), "yyyy-MM-dd"))
+
+# Write the scores data to HDFS in Parquet format
+scores_query = scores_df.writeStream \
+    .format("parquet") \
+    .option("path", SCORES_PATH) \
+    .option("checkpointLocation", f"{CHECKPOINT_DIR}/scores") \
+    .partitionBy("dt") \
+    .outputMode("append") \
+    .start()
+
+# Sink 5: data quality results to HDFS
+dq_df = curated_df.select(
+    col("token").isNotNull().alias("token_not_null"),
+    (col("amount").cast(DoubleType()) > 0).alias("amount_positive"),
+    current_timestamp().alias("dq_run_ts"),
+    date_format(col("ingestion_ts"), "yyyy-MM-dd").alias("dt")
+)
+
+# Write the data quality results to HDFS in Parquet format
+dq_query = dq_df.writeStream \
+    .format("parquet") \
+    .option("path", DQ_PATH) \
+    .option("checkpointLocation", f"{CHECKPOINT_DIR}/dq") \
+    .partitionBy("dt") \
+    .outputMode("append") \
+    .start()
+
+"""
 # Extract fields from the JSON data using the defined schema
 # get_json_object is used to extract specific fields from the JSON string
 # date_format is used to create a date column for partitioning the output data
-txn_df = kafka_df.select(
+txn_df = raw_df.select(
     "event_key",
     "event_json",
     "ingestion_ts",
@@ -86,6 +190,6 @@ query = txn_df.writeStream \
     .partitionBy("date") \
     .outputMode("append") \
     .start()
-
-# Await termination of the streaming query
-query.awaitTermination()
+"""
+# Await termination of all streams
+spark.streams.awaitAnyTermination()
